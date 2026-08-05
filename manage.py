46f9b1a95d0565b5968a810b0d09e790dff9dc6b
Biniam Matheos <binyams@yahoo.com>
2026-08-05 13:27:37 -0700

    Updated ProcFile

--- manage.py
+++ manage.py
@@ -3,10 +3,16 @@
 import os
 import sys
 
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE",
+    "dj_ai_employee_main.settings",
+)
+
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dj_ai_employee_main.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE",
+                          "dj_ai_employee_main.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
